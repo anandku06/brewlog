253a43f0b1d4668353fc7398abcdf242ff883223
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-05-13 16:38:39 +0530

    feat: enhance CoffeeForm and Layout components with structured headers and footers

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { coffeeOptions } from '../utils'
 
 const CoffeeForm = () => {
   return (
-    <div>CoffeeForm</div>
+    <>
+    <div className="section-header">
+      <i className='fa-solid fa-pencil'/>
+      <h2>Start Tracking Today...</h2>
+    </div>
+    <h4>Select Coffee type</h4>
+    <div className='coffee-grid'>
+      {coffeeOptions.slice(0, 5).map((opt, optIndex) => {
+        return (
+          <button className='button-card' key={optIndex}>
+            <h4>{opt.name}</h4>
+            <p>{opt.caffeine} mg</p>
+          </button>
+        )
+      })}
+      <button className='button-card'>
+        <h4>Other</h4>
+        <p>n/a</p>
+      </button>
+    </div>
+
+    <select id="coffee-list" name='coffee-list'>
+      <option value={null}>Select type</option>
+      {coffeeOptions.map((opt, optIndex) => {
+        return (
+          <option value={opt.name} key={optIndex}>
+            {opt.name} ({opt.caffeine} mg)
+          </option>
+        )
+      })}
+    </select>
+    </>
   )
 }
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,9 +2,24 @@ import React from "react";
 
 const Layout = (props) => {
   const { children } = props;
-  const header = <header></header>;
+  const header = (
+    <header>
+      <div>
+        <h1 className="text-gradient">BrewLog</h1>
+        <p>For Coffee Insatiates</p>
+      </div>
+      <button>
+        <p>Sign up for Free</p>
+        <i className="fa-solid fa-mug-hot"></i>
+      </button>
+    </header>
+  );
 
-  const footer = <footer></footer>;
+  const footer = (
+  <footer>
+    <p><span className="text-gradient">BrewLog</span> was made by <a href="https://www.linkedin.com/in/anand-kumar-023231291">Anand</a> using the <a href="http://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.</p>
+  </footer>
+);
   return (
     <>
       {header}
